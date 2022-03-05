56a4bd891d49e751267943b7fd3fe0d8fceadce5
k-takahashi <watoku58@gmail.com>
2022-03-05 07:05:48 +0000

    課題の提出 PHP/Laravel 11 課題3~7

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,17 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+{{-- layouts/profile.blade.phpファイルを読み込む --}}
+@extends('layouts.profile')
 
-        <title>Profile</title>
-    </head>
-    <body>
-        <h1>プロファイル画面</h1>
-    </body>
-</html>
+
+{{-- profile.blade.phpの@yield('title')に'プロフィールの新規作成'を埋め込む --}}
+@section('title', 'myプロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
