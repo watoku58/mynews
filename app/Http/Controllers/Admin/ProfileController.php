5af41989f254066427ce29940b8b633e6c3ade6e
k-takahashi <watoku58@gmail.com>
2022-03-06 02:01:24 +0000

    Admin追加

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+// 以下を追記することでProfile Modelが扱えるようになる
+use App\Profile;
+
+class ProfileController extends Controller
+{
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+
+    public function create(Request $request)//()内に入れているRequestの機能が分からない。
+    {
+        
+        // 以下を追記(PHP/Laravel 14)
+        // Varidationを行う
+        $this->validate($request, Profile::$rules);
+        
+        $profile = new Profile;
+        $form = $request->all();
+        
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        // データベースに保存する
+        $profile->fill($form);
+        $profile->save();
+        
+        return redirect('admin/profile/create');
+    }
+
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
+}
