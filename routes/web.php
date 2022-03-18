e6b79ef96a5bf3f0618e8df5551d664ac187559f
k-takahashi <watoku58@gmail.com>
2022-03-18 08:30:25 +0000

    課題の提出 PHP/Laravel 19

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,16 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create'); # 追記
-    Route::get('profile/create', 'Admin\ProfileController@add')->middleware('auth');
-    Route::post('profile/create', 'Admin\ProfileController@create'); # 追記
-    Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth');
-    Route::post('profile/edit', 'Admin\ProfileController@update'); # 追記
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 
 Auth::routes();
@@ -30,3 +34,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'NewsController@index'); //追記 PHP/Laravel 19
+
+Route::get('/profile', 'ProfileController@index'); //追記 PHP/Laravel 19
