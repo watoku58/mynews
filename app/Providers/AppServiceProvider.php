864d9d44ef2751e50889acaf3a548bec73d8e2dc
k-takahashi <watoku58@gmail.com>
2022-03-18 11:07:27 +0000

    Herokuへのデプロイ DBの設定

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        // 以下を追記 (Herokuへのデプロイ)
+        if (\App::environment('production')) {
+            \URL::forceScheme('https');
+        }
+        
+        
     }
 }
